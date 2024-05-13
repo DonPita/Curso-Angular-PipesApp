72aeb3a1b5fd7a4cfb4af7a9697a0bd13e1c4f0a
Adrian Pita <pitabermudez@gmail.com>
2024-05-13 09:19:53 +0200

    Pipes: uppercase, lowercase y tittle case, con su respectiva modificacion en el componente y la importación del prime-ng.module al products.module

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 
 
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset'
 import { MenubarModule } from 'primeng/menubar';
-
-
+import { PanelModule } from 'primeng/panel'
 
 //Modulo centralizado para importar todo lo del PrimeNG y exportarlo al exterior.
 
 @NgModule({
 
   exports: [
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
     MenubarModule,
-
+    PanelModule
   ]
 }
 )
--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class BasicsPageComponent {
 
+  public nameLower: string = 'adrian';
+  public nameUpper: string = 'ADRIAN';
+  public fullName: string = 'aDRian pItA';
 }
